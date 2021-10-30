753eaa943c72a7ed9b9ab8d6037276a7c17c0b6f
Jenni Mämmi <30490109+JenniMammi@users.noreply.github.com>
2021-10-30 16:53:13 +0300

    Made Forecast work according to search

--- src/Forecast.js
+++ src/Forecast.js
@@ -1,11 +1,15 @@
 import axios from "axios";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./Weather.css";
 import DailyForecast from "./DailyForecast";
 
 export default function Forecast(props) {
   let [loaded, setLoaded] = useState(false);
   let [forecast, setForecast] = useState(null);
+
+  useEffect(() => {
+    setLoaded(false);
+  }, [props.coord]);
 
   function handleResponse(response) {
     setForecast(response.data.daily);
